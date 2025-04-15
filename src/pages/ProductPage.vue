ad2fba556fd0f922545e3b816638229c581ca780
SamChang <fatty720122@gmail.com>
2025-04-15 11:17:44 +0800

    格式化 HTML 和 Vue 組件以提高可讀性

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -1,7 +1,17 @@
-<template><div v-if="product"><img :src="product.image" width="150" /><h2>{{ product.name }}</h2><p>${{ product.price }}</p><button @click="addToCart(product)">Add to Cart</button></div></template><script setup>import { useRoute } from 'vue-router'
-import products from '../assets/products.json'
-import { useCartStore } from '../store/cart.js'
+<template>
+  <div v-if="product">
+    <img :src="product.image" width="150" />
+    <h2>{{ product.name }}</h2>
+    <p>${{ product.price }}</p>
+    <button @click="addToCart(product)">Add to Cart</button>
+  </div>
+</template>
+<script setup>
+import { useRoute } from "vue-router";
+import products from "../assets/products.json";
+import { useCartStore } from "../store/cart.js";
 
-const route = useRoute()
-const product = products.find(p => p.id === parseInt(route.params.id))
-const { addToCart } = useCartStore()</script>
+const route = useRoute();
+const product = products.find((p) => p.id === parseInt(route.params.id));
+const { addToCart } = useCartStore();
+</script>
